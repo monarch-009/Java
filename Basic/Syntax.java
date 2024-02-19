a62e444af603e3edaf38b82dbbcf32901a7f0fcc
monarch-009 <adityasinghmoni@gmail.com>
2024-02-19 14:43:48 +0530

    Syntax  of java

---
+++ Basic/Syntax.java
@@ -0,0 +1,5 @@
+public class Syntax {
+    public static void main(String[] args){ // The main() method is most important in java code because compiler start to execute from here only.
+        System.out.println("Hello Java!"); // println() method print whatever is inside it.
+    }
+}
