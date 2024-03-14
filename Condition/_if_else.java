c20116fd125254896a6d89cd2b6f7bc6ed1c1eda
monarch-009 <adityasinghmoni@gmail.com>
2024-03-14 14:49:28 +0530

    Conditional Statement in java

---
+++ Condition/_if_else.java
@@ -0,0 +1,10 @@
+public class _if_else {
+    public static void main(String[] args) {
+        int i = 10;
+
+        if (i < 15)
+            System.out.println("i is smaller than 15");
+        else
+            System.out.println("i is greater than 15");
+    }
+}
