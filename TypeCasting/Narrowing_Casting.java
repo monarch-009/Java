000dc295519d1c275af9aa7a69903fe317de5c1e
monarch-009 <adityasinghmoni@gmail.com>
2024-03-14 12:51:50 +0530

    Type Casting in Java

---
+++ TypeCasting/Narrowing_Casting.java
@@ -0,0 +1,8 @@
+public class Narrowing_Casting {
+    public static void main(String[] args) {
+        double num = 7.75d;
+        int num1 = (int) num;
+        System.out.println("Double : " + num);
+        System.out.println("Int : " + num1);
+    }
+}
---
+++ TypeCasting/Windening_Casting.java
@@ -0,0 +1,9 @@
+public class Windening_Casting {
+    public static void main(String[] args) {
+        int numInt = 9;
+        double numDouble = numInt;
+        System.out.println("Int : " + numInt);
+        System.out.println("Double : " + numDouble);
+    }
+
+}
