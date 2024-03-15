85293eb92949a9965f144f8aa6b1511188570815
monarch-009 <adityasinghmoni@gmail.com>
2024-03-15 12:42:38 +0530

    Loop in java

---
+++ Loops/for_each_loop.java
@@ -0,0 +1,19 @@
+public class for_each_loop {
+    public static void main(String[] args) {
+        // String array
+        String array[] = { "Ron", "Harry", "Hermoine" };
+
+        // enhanced for loop
+        for (String x : array) {
+            System.out.println(x);
+        }
+
+        /*
+         * for loop for same function
+         * for (int i = 0; i < array.length; i++)
+         * {
+         * System.out.println(array[i]);
+         * }
+         */
+    }
+}
---
+++ Loops/for_loop.java
@@ -0,0 +1,9 @@
+public class for_loop {
+    public static void main(String[] args) {
+        System.out.println("Print Counting:");
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
